8e25fbfc1ace02074fe1e71f2dc372dec1b5998a
rbhandari17 <rbhandari17@myseneca.ca>
2025-01-27 13:28:20 -0500

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define the integer objects
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation (using the order of operations: PEMDAS)
+result = x + y * z
+
+# Print the result in the desired format
+print(f"{x} + {y} * {z} = {result}")
