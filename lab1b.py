f509651fe20ca9f57fc436d776db377adda5ad0c
rbhandari17 <rbhandari17@myseneca.ca>
2025-01-27 13:27:24 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Define the object 'name' with the value 'Isaac'
+name = 'Isaac'
+
+# Print the desired message with the value of 'name'
+print('How old are you ' + name + '?')
+
